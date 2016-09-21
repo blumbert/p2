7a9463c262645119bcfd8749d8b847d8b6b9c1f4
B <B@Bs-MacBook.hsd1.ma.comcast.net>
2016-09-21 14:19:22 -0400

    added styling, removed debugging

--- index.php
+++ index.php
@@ -14,16 +14,18 @@ require 'processing.php';
     <div id="password">
         <span><?php echo $password; ?></span>
     </div>
-    <form id="criteria_form">
-        <label for="f_numwords">Number of words (max 9):</label>
-        <input id="f_numwords" name="numwords" type="text" maxlength="1"><br>
-        <label for="f_number">Include a number</label>
-        <input id="f_number" name="includeNumber" type="checkbox" value="1"><br>
-        <label for="f_symbol">Include a symbol</label>
-        <input id="f_symbol" name="includeSymbol" type="checkbox" value="1"><br>
-        <input type="submit" value="Generate">
-    </form>
-    <div id="message"><?php echo $message; ?></div>
+    <div id="controls">
+        <form id="criteria_form">
+            <label for="f_numwords">Number of words (max 9):</label>
+            <input id="f_numwords" name="numwords" type="text" maxlength="1"><br>
+            <label for="f_number">Include a number</label>
+            <input id="f_number" name="includeNumber" type="checkbox" value="1"><br>
+            <label for="f_symbol">Include a symbol</label>
+            <input id="f_symbol" name="includeSymbol" type="checkbox" value="1"><br>
+            <input type="submit" value="Generate">
+        </form>
+        <div id="message"><?php echo $message; ?></div>
+    </div>
     <footer>
         <script src="js/scripts.js"></script>
     </footer>
--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 $('#criteria_form').submit(function(e) {
     // get the number of words requested
     var numWords = $('#f_numwords').val();
-alert(numWords);
+    
     if (isNaN(numWords) || numWords > 9) {
         $('#message').text("Invalid number given.");
         e.preventDefault();
