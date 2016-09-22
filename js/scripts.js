a967ca76fa4eb90041f01462181fd392f95e8099
B <B@Bs-MacBook.hsd1.ma.comcast.net>
2016-09-22 12:57:51 -0400

    increased client-side validation

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $('#criteria_form').submit(function(e) {
     // get the number of words requested
     var numWords = $('#f_numwords').val();
     
-    if (isNaN(numWords) || numWords > 9) {
+    if (numWords == "" || isNaN(numWords) || numWords > 9) {
         $('#message').text("Invalid number given.");
         e.preventDefault();
     }
